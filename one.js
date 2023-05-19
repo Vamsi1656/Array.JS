50a67d87bd971964995ccb007049247f9f6b658a
Vamsi1656 <vamsireddy1717@gmail.com>
2023-05-19 12:01:54 +0530

    second push

---
+++ one.js
@@ -0,0 +1,48 @@
+/*group of values as one entity
+let a=[10,20,30,40]
+console.log(a)
+*/
+/*it allows duplicates
+let a=[10,20,10,20,10,30,40]
+console.log(a)
+*/
+/*allowed heterogenous data elements
+let a =[10,"Nani",9.5,true,{}]
+console.log(a)
+*/
+/*it has strong indexing
+let a=[10,20,30,40]
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+*/
+/*array is iterable object in JS
+let enames = ["Nani","Pallavi","Virat","DSP"]
+for(i=0;i<=3;i++)
+console.log(enames[i]
+
+let enames = ["Nani","Pallavi","Virat","DSP"]
+i=0
+while(i<=3){
+    console.log(enames[i])
+    i++
+}
+*/
+/*for of is used in Array 
+let enames = ["Nani","Pallavi","Virat","DSP"]
+for(ename of enames){
+    console.log(ename)
+}*/
+let ids = [101, 102, 103, 104, 105]
+
+ for (id of ids) {
+    console.log(id)
+} 
+
+/*let i = 0;
+while (i <= ids.length - 1) {
+    console.log(ids[i])
+
+    i++
+}*/
---
+++ two.js
@@ -0,0 +1,21 @@
+/*how to create arrays in java script?
+let cars =["bmw","benz","ferari","fortuner"]
+console.log(cars)
+*/
+/*How to access items from arrays in Js?
+(By using Indexes)
+
+let airplanes=["Indigo","Emirates","Spicejet"]
+ console.log(airplanes[0]);
+ */
+/*How to access all items from array using for loop in JS?
+let fruits=["Mango","Chikoo","Apple","Kiwi"]
+for(i=0;i<=3;i++){
+    console.log(fruits[i])
+}*/
+/*Multi dimensional array
+
+let ename=[["Nani",30],["pallavi",29],["Virat",33]]
+for(i=0;i<=2;i++)
+console.log(ename[i])
+*/
